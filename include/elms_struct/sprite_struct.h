8acfe6858f9ff56b927f4ca4a9e6368b2f6d62fb
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-02-18 11:15:05 +0100

    feat(): Add struct for animation sprite

---
+++ include/elms_struct/sprite_struct.h
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2023
+** sprite_struct
+** File description:
+** sprite_struct
+*/
+
+#ifndef SPRITE_STRUCT_
+    #define SPRITE_STRUCT_
+
+    #include <SFML/Graphics.h>
+
+typedef struct m_sprite_struct{
+    sfTexture *texture;
+    sfSprite *sprite;
+    sfVector2f scale;
+    sfVector2f pos;
+    sfIntRect rect;
+    sfClock *clock;
+    sfTime time;
+    float seconds;
+} s_sprite;
+
+#endif /* !SPRITE_STRUCT_ */
